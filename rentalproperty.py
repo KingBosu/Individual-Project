c16bf898eda3fba115e5eee6e7cb7b3e07a94fc4
KingBosu <mckaywilliamiii@gmail.com>
2023-05-14 18:37:56 -0400

    Added a runner function and tried to fix up code

--- rentalproperty.py
+++ rentalproperty.py
@@ -1,10 +1,11 @@
 
 # Income types that will be contributing to ROI
 class Income:
-    def __init__(self, rental_income, total_income, storage, misc):
+    def __init__(self, rental_income, storage, misc):
         self.rental_income = rental_income
-
-        self.total_income = []
+        self.storage = storage
+        self.misc = misc
+        self.total_income = sum(self.rental_income) + sum(self.storage) + sum(misc)
 
     # Cost of expenses for the property
 
@@ -20,17 +21,19 @@ class Expenses:
         self.capital_expenses = capital_expenses
         self.property_management = property_management
         self.mortgage = mortgage
+        self.total_expenses = sum(self.taxes + self.insurance + self.utilities + hoa_fees + grounds_keeping + vacancy + capital_expenses + property_management + mortgage)
 
 
 class RoiCalculator:
-    def __init__(self, Income, Expenses):
-        self.Income = Income
-        self.Expenses = Expenses
+    def __init__(self):
+        self.Income = self.total_income
+        self.Expenses = self.total_expenses
 
     # income - expenses
     def cashflow(self):
-        total_monthly_caseflow = self.Income.total
-        annual_cash_flow = self.Expenses.total * 12
+        self.total_monthly_cashflow = self.Income.total - self.Expenses.total
+        self.annual_cash_flow = self.total_monthly_cashflow * 12
+        return self.annual_cash_flow
 
     # Cash on Cash ROI = what percentage your money is earning
     def cash_roi(self):
@@ -40,4 +43,30 @@ class RoiCalculator:
         misc_others = []
         total_investment = sum(down_payment) + sum(closing_cost) + \
             sum(rehab_budget) + sum(misc_others)
-        cash_roi = annual_cash_flow // total_investment
+        cash_roi = (self.annual_cash_flow // total_investment) * 100
+        return cash_roi
+    
+    def run_roi_calculator():
+    
+        rental_income = [1000, 2000, 1200]
+        storage_income = [0, 0, 100]
+        misc_income = [50, 0, 100]
+        income = Income(rental_income, storage_income, misc_income)
+
+        taxes = 1000
+        insurance = 500
+        utilities = 300
+        hoa_fees = 0
+        grounds_keeping = 200
+        vacancy = 250
+        capital_expenses = 150
+        property_management = 300
+        mortgage = 1500
+        expenses = Expenses(taxes, insurance, utilities, hoa_fees, grounds_keeping, vacancy, capital_expenses, property_management, mortgage)
+
+        roi_calculator = RoiCalculator(income, expenses)
+        monthly_cashflow = roi_calculator.cashflow()
+        cash_roi = roi_calculator.cash_roi()
+
+    print("Monthly cash flow: $", self.total_monthly_cashflow)
+    print("Cash on cash ROI: {:.2f}%".format(cash_roi))
